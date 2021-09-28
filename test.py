0033bca7bf1d2255db2deb849e6e5ec40cc05713
lauris.schleussner <lauris.schleussner@gmail.com>
2021-09-28 19:01:00 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,86 @@
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import tensorflow as tf
+import glob
+
+AUTOTUNE = tf.data.AUTOTUNE
+batch_size = 32
+
+image_count = len(list(glob.glob('D:/BELL/wikiart-dataset/wikiart/*/*.jpg')))
+# mage_count = 200
+
+list_ds = tf.data.Dataset.list_files(str("D:/BELL/wikiart-dataset/wikiart/*/*"), shuffle=True)
+list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
+
+class_names = np.array(os.listdir('D:/BELL/wikiart-dataset/wikiart'))
+np.sort(class_names)
+
+val_size = int(image_count * 0.2)
+train_ds = list_ds.skip(val_size)
+val_ds = list_ds.take(val_size)
+
+"""Convert a file path to (img, label) pair"""
+def get_label(file_path):
+    # convert the path to a list of path components
+    parts = tf.strings.split(file_path, os.path.sep)
+    # The second to last is the class-directory
+    one_hot = parts[-2] == class_names
+    # Integer encode the label
+    return tf.argmax(one_hot)
+
+def decode_img(img):
+    img_height = 100
+    img_width = 100
+    # convert the compressed string to a 3D uint8 tensor
+    img = tf.io.decode_jpeg(img, channels=3)
+    # resize the image to the desired size
+    return tf.image.resize(img, [img_height, img_width])
+
+def process_path(file_path):
+  label = get_label(file_path)
+  # load the raw data from the file as a string
+  img = tf.io.read_file(file_path)
+  img = decode_img(img)
+  return img, label
+
+train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+for image, label in train_ds.take(1):
+  print("Image shape: ", image.numpy().shape)
+  print("Label: ", label.numpy())
+
+def configure_for_performance(ds):
+  ds = ds.cache()
+  ds = ds.shuffle(buffer_size=1000)
+  ds = ds.batch(batch_size)
+  ds = ds.prefetch(buffer_size=AUTOTUNE)
+  return ds
+
+train_ds = configure_for_performance(train_ds)
+val_ds = configure_for_performance(val_ds)
+
+model = tf.keras.Sequential([
+  tf.keras.layers.experimental.preprocessing.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(class_names.size)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=3
+)
+
+keras_model_path = "/tmp/keras_save"
+model.save(keras_model_path)
