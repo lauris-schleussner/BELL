bd28eb676f0d431861b6a2e9799b8a2d6f5faa63
lauris-schleussner <79599893+lauris-schleussner@users.noreply.github.com>
2021-11-05 23:30:19 +0100

    new

--- confusionmatrix.py
+++ confusionmatrix.py
@@ -1,33 +1,36 @@
-# https://www.machinecurve.com/index.php/2020/02/21/how-to-predict-new-samples-with-your-keras-model/
-
-import tensorflow as tf
-import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # silence
 import numpy as np
 from tensorflow.keras.models import Sequential, save_model, load_model
+import prototype
 
-filepath = "models"
+# get data from the other script
+model, orderlist, test_ds = prototype.main()
 
-model = load_model(filepath, compile = True)
+# might work with batching
+images, labels = tuple(zip(*test_ds)) 
+# images = tf.data.Dataset.from_tensor_slices(images)
+# images = images.batch(32, drop_remainder = True)
 
-def decode_img(img):
-  img_height = 100
-  img_width = 100
-  # Convert the compressed string to a 3D uint8 tensor
-  img = tf.io.decode_jpeg(img, channels=3)
-  # Resize the image to the desired size
-  return tf.image.resize(img, [img_height, img_width])
+labels = np.array(labels)
+labels = labels.flatten()
+print("labels: ", labels)
 
-def process_path(file_path):
-  # Load the raw data from the file as a string
-  img = tf.io.read_file(file_path)
-  img = decode_img(img)
-  return img
+rawpredictions = model.predict(images)
 
+argmaxpredictions = tf.argmax(rawpredictions, axis=-1)
+argmaxpredictions = argmaxpredictions.numpy()
+argmaxpredictions = argmaxpredictions.flatten()
+print("argmaxpredictions:", argmaxpredictions)
 
-input = process_path("D:/BELL/IADataset/Impressionism/abdullah-suriosubroto_air-terjun.jpg")
-input = 
+weights = []
 
-print(input.get_shape())
-model.summary()
-predictions = model.predict(input)
-print(predictions)
+for i in range(len(rawpredictions)):
+    rawsingleprediction = rawpredictions[i]
+    argmaxindex = argmaxpredictions[i]
+    weights.append(rawsingleprediction[argmaxindex])
+
+print("weights", weights)
+
+confusion = tf.math.confusion_matrix(labels=labels, predictions=argmaxpredictions, weights = weights)
+print(confusion)
