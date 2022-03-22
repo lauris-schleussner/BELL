fbd54998fe331e22a7a68cdfcd218c5ea714febf
lauris-schleussner <79599893+lauris-schleussner@users.noreply.github.com>
2022-03-22 13:30:52 +0100

    now supports being called from other script

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -7,129 +7,129 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint
 import tensorflow.keras.backend as K
 import numpy as np
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 import pickle
-import pandas as pd
 import os
 
 # https://www.analyticsvidhya.com/blog/2021/01/querying-similar-images-with-tensorflow/
 
-# Load images
-img_height = 244
-img_width = 244
-channels = 3
-batch_size = 16
+def main():
 
-train_datagen = ImageDataGenerator(rescale=1./255,
-                                   validation_split=0.2)
+  # Load images
+  img_height = 244
+  img_width = 244
+  channels = 3
+  batch_size = 16
 
-training_set = train_datagen.flow_from_directory(
-    './autoenc',
-     target_size = (img_height, img_width),
-     batch_size = batch_size,
-     class_mode = 'input',
-     subset = 'training',
-     shuffle=True)
+  train_datagen = ImageDataGenerator(rescale=1./255,
+                                    validation_split=0.2)
 
-validation_set = train_datagen.flow_from_directory(
-    './autoenc',
-     target_size = (img_height, img_width),
-     batch_size = batch_size,
-     class_mode = 'input',
-     subset = 'validation',
-     shuffle=False)
+  training_set = train_datagen.flow_from_directory(
+      './autoenc',
+      target_size = (img_height, img_width),
+      batch_size = batch_size,
+      class_mode = 'input',
+      subset = 'training',
+      shuffle=True)
 
-if not os.path.isdir("autoenc/resized"):
-  raise ValueError("No folder found, was the 'resized' folder copied to 'autoenc/resized'?")
+  validation_set = train_datagen.flow_from_directory(
+      './autoenc',
+      target_size = (img_height, img_width),
+      batch_size = batch_size,
+      class_mode = 'input',
+      subset = 'validation',
+      shuffle=False)
 
-# Define the autoencoder
-input_model = Input(shape=(img_height, img_width, channels))
+  if not os.path.isdir("autoenc/resized"):
+    raise ValueError("No folder found, was the 'resized' folder copied to 'autoenc/resized'?")
 
-# Encoder layers
-encoder = Conv2D(32, (3,3), padding='same', kernel_initializer='normal')(input_model)
-encoder = LeakyReLU()(encoder)
-encoder = BatchNormalization(axis=-1)(encoder)
+  # Define the autoencoder
+  input_model = Input(shape=(img_height, img_width, channels))
 
-encoder = Conv2D(64, (3,3), padding='same', kernel_initializer='normal')(encoder)
-encoder = LeakyReLU()(encoder)
-encoder = BatchNormalization(axis=-1)(encoder)
+  # Encoder layers
+  encoder = Conv2D(32, (3,3), padding='same', kernel_initializer='normal')(input_model)
+  encoder = LeakyReLU()(encoder)
+  encoder = BatchNormalization(axis=-1)(encoder)
 
-encoder = Conv2D(64, (3,3), padding='same', kernel_initializer='normal')(input_model)
-encoder = LeakyReLU()(encoder)
-encoder = BatchNormalization(axis=-1)(encoder)
+  encoder = Conv2D(64, (3,3), padding='same', kernel_initializer='normal')(encoder)
+  encoder = LeakyReLU()(encoder)
+  encoder = BatchNormalization(axis=-1)(encoder)
 
-encoder_dim = K.int_shape(encoder)
-encoder = Flatten()(encoder)
+  encoder = Conv2D(64, (3,3), padding='same', kernel_initializer='normal')(input_model)
+  encoder = LeakyReLU()(encoder)
+  encoder = BatchNormalization(axis=-1)(encoder)
 
-# Latent Space
-latent_space = Dense(16, name='latent_space')(encoder)
+  encoder_dim = K.int_shape(encoder)
+  encoder = Flatten()(encoder)
 
-# Decoder Layers
-decoder = Dense(np.prod(encoder_dim[1:]))(latent_space)
-decoder = Reshape((encoder_dim[1], encoder_dim[2], encoder_dim[3]))(decoder)
+  # Latent Space
+  latent_space = Dense(16, name='latent_space')(encoder)
 
-decoder = Conv2DTranspose(64, (3,3), padding='same', kernel_initializer='normal')(decoder)
-decoder = LeakyReLU()(decoder)
-decoder = BatchNormalization(axis=-1)(decoder)
+  # Decoder Layers
+  decoder = Dense(np.prod(encoder_dim[1:]))(latent_space)
+  decoder = Reshape((encoder_dim[1], encoder_dim[2], encoder_dim[3]))(decoder)
 
-decoder = Conv2DTranspose(64, (3,3), padding='same', kernel_initializer='normal')(decoder)
-decoder = LeakyReLU()(decoder)
-decoder = BatchNormalization(axis=-1)(decoder)
+  decoder = Conv2DTranspose(64, (3,3), padding='same', kernel_initializer='normal')(decoder)
+  decoder = LeakyReLU()(decoder)
+  decoder = BatchNormalization(axis=-1)(decoder)
 
-decoder = Conv2DTranspose(32, (3,3), padding='same', kernel_initializer='normal')(decoder)
-decoder = LeakyReLU()(decoder)
-decoder = BatchNormalization(axis=-1)(decoder)
+  decoder = Conv2DTranspose(64, (3,3), padding='same', kernel_initializer='normal')(decoder)
+  decoder = LeakyReLU()(decoder)
+  decoder = BatchNormalization(axis=-1)(decoder)
 
-decoder = Conv2DTranspose(3, (3, 3), padding="same")(decoder)
-output = Activation('sigmoid', name='decoder')(decoder)
+  decoder = Conv2DTranspose(32, (3,3), padding='same', kernel_initializer='normal')(decoder)
+  decoder = LeakyReLU()(decoder)
+  decoder = BatchNormalization(axis=-1)(decoder)
 
-# Create model object
-autoencoder = Model(input_model, output, name='autoencoder')
+  decoder = Conv2DTranspose(3, (3, 3), padding="same")(decoder)
+  output = Activation('sigmoid', name='decoder')(decoder)
 
-# Compile the model
-autoencoder.compile(loss="mse", optimizer= Adam(learning_rate=1e-3))
+  # Create model object
+  autoencoder = Model(input_model, output, name='autoencoder')
 
-# Fit the model
-history = autoencoder.fit_generator(
-          training_set,
-          steps_per_epoch=training_set.n // batch_size,
-          epochs = 300,
-          validation_data=validation_set,
-          validation_steps=validation_set.n // batch_size,
-          callbacks = [ModelCheckpoint('autoenc/models/image_autoencoder_2.h5', 
-                                       monitor='val_loss', 
-                                       verbose=0, 
-                                       save_best_only=True, 
-                                       save_weights_only=False)])
+  # Compile the model
+  autoencoder.compile(loss="mse", optimizer= Adam(learning_rate=1e-3))
 
-autoencoder.summary()
+  # Fit the model
+  history = autoencoder.fit_generator(
+            training_set,
+            steps_per_epoch=training_set.n // batch_size,
+            epochs = 300,
+            validation_data=validation_set,
+            validation_steps=validation_set.n // batch_size,
+            callbacks = [ModelCheckpoint('autoenc/models/image_autoencoder_2.h5', 
+                                        monitor='val_loss', 
+                                        verbose=0, 
+                                        save_best_only=True, 
+                                        save_weights_only=False)])
 
-latent_space_model = Model(
-                      autoencoder.input, 
-                      autoencoder.get_layer('latent_space').output)
+  autoencoder.summary()
 
-# Load all images and predict them with the latent space model
-X = []
-indices = []
+  latent_space_model = Model(
+                        autoencoder.input, 
+                        autoencoder.get_layer('latent_space').output)
 
-for i in tqdm(range(len(os.listdir('./autoenc/resized')))):
-  try:
-    img_name = os.listdir('./autoenc/resized')[i]
-    img = load_img('./autoenc/resized/{}'.format(img_name), 
-                   target_size = (244, 244))
-    img = img_to_array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
-    pred = latent_space_model.predict(img)
-    pred = np.resize(pred, (16))
-    X.append(pred)
-    indices.append(img_name)
+  # Load all images and predict them with the latent space model
+  X = []
+  indices = []
 
-  except Exception as e:
-    print(img_name)
-    print(e)
+  for i in tqdm(range(len(os.listdir('./autoenc/resized')))):
+    try:
+      img_name = os.listdir('./autoenc/resized')[i]
+      img = load_img('./autoenc/resized/{}'.format(img_name), 
+                    target_size = (244, 244))
+      img = img_to_array(img) / 255.0
+      img = np.expand_dims(img, axis=0)
+      pred = latent_space_model.predict(img)
+      pred = np.resize(pred, (16))
+      X.append(pred)
+      indices.append(img_name)
 
-    # Export the embeddings
-embeddings = {'indices': indices, 'features': np.array(X)}
-pickle.dump(embeddings, 
-            open('./autoenc/image_embeddings.pickle', 'wb'))
+    except Exception as e:
+      print(img_name)
+      print(e)
+
+      # Export the embeddings
+  embeddings = {'indices': indices, 'features': np.array(X)}
+  pickle.dump(embeddings, 
+              open('./autoenc/image_embeddings.pickle', 'wb'))
