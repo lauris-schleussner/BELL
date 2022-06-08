3f1dee3dba35e39722da36292333f884f97e4328
yur74957 <yuri.cassio.campbell.borges@imw.fraunhofer.de>
2022-06-08 20:14:55 +0200

    wikimaster removed

--- wikiart-master/wikiart.py
+++
@@ -1,4 +0,0 @@
-from wikiart.console import main
-
-if __name__ == '__main__':
-    main()
--- wikiart-master/wikiart/__init__.py
+++
@@ -1,6 +0,0 @@
-"""WikiArt Retriever.
-
-Author: Lucas David -- <ld492@drexel.edu>
-License: MIT License (c) 2016
-
-"""
