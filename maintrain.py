94c1de0498af74fa2bbdc1c858bce503a0ab5c57
yur74957 <yuri.cassio.campbell.borges@imw.fraunhofer.de>
2022-06-09 15:27:56 +0200

    final for compruns

--- maintrain.py
+++ maintrain.py
@@ -10,50 +10,55 @@ import os
 import distutils.dir_util
 
 
-PARAM_EPOCHS = 5 # 200
+PARAM_EPOCHS = 200 # 200
 AE_PARAM_EPOCHS = 3
-PARAM_WAB = False
+PARAM_WAB = True
+GROUP_RUN_TAG = 'comprun03'
+TAGS = [GROUP_RUN_TAG]
+
+os.system('rm -r models')
+os.system('rm -r visualisations_out')
 
 # if not os.path.isdir("autoenc/resized"):
 #     print("copying './resized' to './autoenc/resized' ")
 #     distutils.dir_util.copy_tree("./resized", "./autoenc/resized")
 #     print("copy done")
 
-# if not os.path.isdir("visualisations_out"):
-#     print("creating visualisation directory")
-#     os.mkdir("visualisations_out")
+if not os.path.isdir("visualisations_out"):
+    print("creating visualisation directory")
+    os.mkdir("visualisations_out")
 
 # first network
 print("training cnn1")
-cfm_history.main(train_cnn.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB), plottitle="CNN 1")
+cfm_history.main(train_cnn.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, add_tags=TAGS), plottitle="CNN 1")
 
 # alexnet
-# print("training alexnet")
-# cfm_history.main(train_alexnet.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB), plottitle="AlexNet")
+print("training alexnet")
+cfm_history.main(train_alexnet.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, add_tags=TAGS), plottitle="AlexNet")
 
-# # vgg16 no weights
-# print("training vgg16 without weights")
-# cfm_history.main(train_vgg16.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=False), plottitle="VGG16 newly trained")
+# vgg16 no weights
+print("training vgg16 without weights")
+cfm_history.main(train_vgg16.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=False, add_tags=TAGS), plottitle="VGG16 newly trained")
 
-# # vgg16 with weights
-# print("training vgg16 with pretrained weights")
-# cfm_history.main(train_vgg16.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=True), plottitle="VGG16 pretrained")
+# vgg16 with weights
+print("training vgg16 with pretrained weights")
+cfm_history.main(train_vgg16.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=True, add_tags=TAGS), plottitle="VGG16 pretrained")
 
-# # resnet no weights
-# print("training resnet50 without weights")
-# cfm_history.main(train_resnet50.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=False), plottitle="ResNet50 newly trained")
+# resnet no weights
+print("training resnet50 without weights")
+cfm_history.main(train_resnet50.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=False, add_tags=TAGS), plottitle="ResNet50 newly trained")
 
-# # resnet pretrained weights
-# print("training resnet50 with pretrained weigths")
-# cfm_history.main(train_resnet50.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=True), plottitle="ResNet50 pretrained")
+# resnet pretrained weights
+print("training resnet50 with pretrained weigths")
+cfm_history.main(train_resnet50.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=True, add_tags=TAGS), plottitle="ResNet50 pretrained")
 
-# # xception new weights
-# print("training xception without weigths")
-# cfm_history.main(train_xception.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=False), plottitle="Xception newly trained")
+# xception new weights
+print("training xception without weigths")
+cfm_history.main(train_xception.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=False, add_tags=TAGS), plottitle="Xception newly trained")
 
-# # xception pretrained weights
-# print("training xception with pretrained weigths")
-# cfm_history.main(train_xception.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=True), plottitle="Xception pretrained")
+# xception pretrained weights
+print("training xception with pretrained weigths")
+cfm_history.main(train_xception.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=True, add_tags=TAGS), plottitle="Xception pretrained")
 
 # print("training encoder")
 # train_autoencoder.main(EPOCHS = AE_PARAM_EPOCHS)
