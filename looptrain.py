38f1bad540a256a5e11a2b6480fd874d2079e9bf
yur74957 <yuri.cassio.campbell.borges@imw.fraunhofer.de>
2022-06-09 15:31:27 +0200

    loop of 10

--- looptrain.py
+++ looptrain.py
@@ -15,14 +15,15 @@ PARAM_WAB = True
 GROUP_RUN_TAG = 'looprun01'
 TAGS = [GROUP_RUN_TAG]
 
-# first network
-print("training cnn1")
-train_cnn.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, add_tags=TAGS)
+for i in range(10):
+    # first network
+    print("training cnn1")
+    train_cnn.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, add_tags=TAGS)
 
-# alexnet
-print("training alexnet")
-train_alexnet.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, add_tags=TAGS)
+    # alexnet
+    print("training alexnet")
+    train_alexnet.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, add_tags=TAGS)
 
-# vgg16 no weights
-print("training vgg16 without weights")
-train_vgg16.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=False, add_tags=TAGS)
+    # vgg16 no weights
+    print("training vgg16 without weights")
+    train_vgg16.main(EPOCHS=PARAM_EPOCHS, WAB_FLAG=PARAM_WAB, pretrained=False, add_tags=TAGS)
