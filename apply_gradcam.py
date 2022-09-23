2a8a9ea474879ab9b2937c5a7085ef669d5551fa
Lauris Schleussner <lauris.schleussner@gmail.comr>
2022-09-23 15:15:29 +0200

    gradcam working

--- apply_gradcam.py
+++ apply_gradcam.py
@@ -1,48 +1,93 @@
-from bellutils.gradcam import GradCAM
-import cv2
 import numpy as np
 import tensorflow as tf
-from matplotlib import pyplot as plt
+from tensorflow import keras
+
+# Display
+from IPython.display import Image, display
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+imgpath = "E:/BELL/resized/185058.jpg"
+modelpath = "E:/BELL/models/saved_xception"
+size = (244,244)
+last_conv_layer_name = "block14_sepconv2_act"
+
+# input path and filename under which resulting img should be saved
+def main(imgpath, filename):
+
+    # `img` is a PIL image of size 299x299
+    img = keras.preprocessing.image.load_img(imgpath, target_size=size)
+    # `array` is a float32 Numpy array of shape (299, 299, 3)
+    img_array = keras.preprocessing.image.img_to_array(img)
+    # We add a dimension to transform our array into a "batch"
+    # of size (1, 299, 299, 3)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = keras.applications.xception.preprocess_input(img_array)
 
 
-imgpath = "E:/BELL/resized/184897.jpg"
-modelpath = "E:/BELL/models/05_10_2022_15_52_25/saved_vgg16"
+    model = keras.models.load_model(modelpath)
+
+    # Remove last layer's softmax
+    model.layers[-1].activation = None
+
+    # First, we create a model that maps the input image to the activations
+    # of the last conv layer as well as the output predictions
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    # Then, we compute the gradient of the top predicted class for our input image
+    # with respect to the activations of the last conv layer
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
+
+    # This is the gradient of the output neuron (top predicted or chosen)
+    # with regard to the output feature map of the last conv layer
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+
+    # This is a vector where each entry is the mean intensity of the gradient
+    # over a specific feature map channel
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # We multiply each channel in the feature map array
+    # by "how important this channel is" with regard to the top predicted class
+    # then sum all the channels to obtain the heatmap class activation
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    heatmap = heatmap.numpy()
+
+    img = keras.preprocessing.image.load_img(imgpath)
+    img = keras.preprocessing.image.img_to_array(img)
 
 
-image = tf.io.read_file(imgpath)
-image = tf.image.decode_jpeg(image, channels=3)
-image /= 255 # normalize to [0,1] range
-image = np.expand_dims(image, axis=0)
+    # Rescale heatmap to a range 0-255
+    heatmap = np.uint8(255 * heatmap)
 
+    # Use jet colormap to colorize heatmap
+    jet = cm.get_cmap("jet")
 
-# load model
-model = tf.keras.models.load_model(modelpath)
+    # Use RGB values of the colormap
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
 
+    # Create an image with RGB colorized heatmap
+    jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
 
-# get layer names
-for idx in range(len(model.layers)):
-  print(model.get_layer(index = idx).name)
+    # Superimpose the heatmap on original image
+    superimposed_img = jet_heatmap * 0.5 + img
+    superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
 
+    #superimposed_img.show()
 
-# predict
-preds = model.predict(image) 
-i = np.argmax(preds[0])
+    # Save the superimposed image
+    superimposed_img.save("gcresults/gradcamresults_ " + str(filename) + ".jpg")
 
-# load gradcam class
-icam = GradCAM(model, i)
-heatmap = icam.compute_heatmap(image)
-heatmap = cv2.resize(heatmap, (244, 244))
-
-image = cv2.imread(imgpath)
-image = cv2.resize(image, (244, 244))
-print(heatmap.shape, image.shape)
-
-(heatmap, output) = icam.overlay_heatmap(heatmap, image, alpha=0.5)
-
-fig, ax = plt.subplots(1, 3)
-
-ax[0].imshow(heatmap)
-ax[1].imshow(image)
-ax[2].imshow(output)
-
-plt.show()
+    return superimposed_img
